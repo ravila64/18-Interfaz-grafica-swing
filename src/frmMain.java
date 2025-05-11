80723fddacce59ac7cd6e1ecb26994c204258ab1
Rene Avila <rene.avila.alonso@gmail.com>
2025-05-11 18:10:31 -0500

    examples actionListener

--- src/frmMain.java
+++ src/frmMain.java
@@ -1,28 +1,43 @@
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class frmMain {
-   private JTextField textField1;
-   private JTextField textField2;
-   private JButton btnInsert;
-   private JButton btnEdit;
-   private JButton btnDelete;
-   private JButton btnExit;
-   private JPanel jPanelPrincipal;
+    private JTextField txtUser;
+    private JButton btnInsert;
+    private JButton btnEdit;
+    private JButton btnDelete;
+    private JButton btnExit;
+    private JPanel jPanelPrincipal;
+    private JLabel lblTexto;
+    private JButton btnMensaje;
+    private JPasswordField txtPassword;
 
-   public frmMain() {
-      btnInsert.addActionListener(new ActionListener() {
-         @Override
-         public void actionPerformed(ActionEvent e) {
-            // null, no tiene un componente padre
-            JOptionPane.showMessageDialog(null,"Hola mundo btn INSERT !!!");
-         }
-      });
-   }
+    public frmMain() {
+        btnInsert.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // null, no tiene un componente padre
+                JOptionPane.showMessageDialog(null, "Hola mundo btn INSERT !!!");
+            }
+        });
+        btnExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        btnMensaje.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String user = txtUser.getText();
+                String mensaje = "Bienvenido " + user + " !!! ";
+                lblTexto.setText(mensaje);
+            }
+        });
+    }
 
-   public JPanel getjPanelPrincipal() {
-      return jPanelPrincipal;
-   }
+    public JPanel getjPanelPrincipal() {
+        return jPanelPrincipal;
+    }
 }
